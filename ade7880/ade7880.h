f152aca3eee05c9611b4e68540f9da9f45cffaf9
Michaël Piron <4469877+michaelpiron@users.noreply.github.com>
2021-06-12 15:33:02 +0200

    Corrected the update procedure for ADE7880
    Corrected the update procedure for ADE7880 according to specification

--- ade7880/ade7880.h
+++ ade7880/ade7880.h
@@ -28,18 +28,98 @@ class ADE7880 : public i2c::I2CDevice, public PollingComponent {
   void set_active_power_c_sensor(sensor::Sensor *active_power_c_sensor) {
     active_power_c_sensor_ = active_power_c_sensor;
   }
-
+  
+  /**
+  * @brief   Quickly calculate the log base 2 of a number for the PGA gains
+  * */
+  static uint32_t quick_log2(uint32_t num)
+  {
+    uint32_t log = 0;
+    while (num >>= 1)
+      ++log;
+    return log;
+  }
+  
+  
   void setup() override {
     if (this->has_irq_) {
       auto pin = GPIOPin(this->irq_pin_number_, INPUT);
       this->irq_pin_ = &pin;
       this->irq_pin_->setup();
     }
-   /* Check the register definitions on page 60 of the ADE chip specification ( https://www.analog.com/media/en/technical-documentation/data-sheets/ADE7953.pdf ) */
+    
+    /* Setup procedure ADE7880 according to specification ( https://www.analog.com/media/en/technical-documentation/data-sheets/ADE7880.pdf ) */
     this->set_timeout(100, [this]() {
-      this->ade_write_<uint8_t>(0x0010, 0x04); /* to check */
-      this->ade_write_<uint8_t>(0x00FE, 0xAD); /* Part of power-up procedure: writing 0xAD to Register Addresss 0xFE unlocks Register 0x120 (pg18) */
-      this->ade_write_<uint16_t>(0x0120, 0x0030); /* Part of power-up procedure: Register 0x120 ensures that the optimum timing configuration is selected to maximize the accuracy and dynamic range.(pg18) */
+      // Configure ADE GPIOs
+      
+      // Reset ADE
+      
+      // Set Power Mode
+      
+      // Choose I2C as main interface and lock
+      this->ade_write_<uint8_t>(ADE_CONFIG2, 0x02);
+      
+      // Set gains
+      uint16_t gain;
+      
+      gain =  (quick_log2(ADE_PGA_VGAIN) << 6) | 
+              (quick_log2(ADE_PGA_NGAIN) << 3) | 
+              (quick_log2(ADE_PGA_IGAIN) << 0);
+      
+      ade_write_<uint16_t>(ADE_Gain, gain);
+      
+      // Set CONFIG register
+#if ADE_USE_ROGOWSKI
+      this->ade_write_<uint32_t>(ADE_DICOEFF, 0xFF8000);
+      this->ade_write_<uint16_t>(ADE_CONFIG, 0x0001); /* Not sure about number of bits */
+#endif
+      // Set COMPMODE register
+#if ADE_USE_60HZ
+      /* Default value + bit 14 set to 1*/
+      this->ade_write_<uint16_t>(ADE_COMPMODE, 0x41FF);
+#endif
+      
+      // Initialize all the other data memory RAM registers
+      
+      // Initialize the WTHR, VARTHR, VATHR, VLEVEL andVNOM registers
+      this->ade_write_<uint8_t>(ADE_WTHR, ADE_WTHR_VAL);
+      this->ade_write_<uint8_t>(ADE_VARTHR, ADE_VARTHR_VAL);
+      this->ade_write_<uint8_t>(ADE_VATHR, ADE_VATHR_VAL);
+      this->ade_write_<uint32_t>(ADE_VLEVEL, ADE_VLEVEL_VAL);
+      this->ade_write_<uint32_t>(ADE_VNOM, ADE_VNOM_VAL);
+      
+      // Initialize CF1DEN, CF2DEN, and CF3DEN
+      this->ade_write_<uint16_t>(ADE_CF1DEN, ADE_CFXDEN_VAL);
+      this->ade_write_<uint16_t>(ADE_CF2DEN, ADE_CFXDEN_VAL);
+      this->ade_write_<uint16_t>(ADE_CF3DEN, ADE_CFXDEN_VAL);
+      
+      // Enable RAM protection
+      this->ade_write_<uint8_t>(0xE7FE, 0xAD);
+      this->ade_write_<uint8_t>(0xE7E3, 0x80);
+      
+      // Read back all data memory RAM registers to ensure thatthey initialized with the desired values.
+      
+      // Start the DSP
+      this->ade_write_<uint16_t>(ADE_RUN, 0x0001);
+      
+      // Read the energy registers xWATTHR, xVAHR, xFWATTHR, and xFVARHR to erase their content and start energyaccumulation from a known state.
+      this->ade_read_<uint32_t>(ADE_AWATTHR);
+      this->ade_read_<uint32_t>(ADE_BWATTHR);
+      this->ade_read_<uint32_t>(ADE_CWATTHR);
+      this->ade_read_<uint32_t>(ADE_AVAHR);
+      this->ade_read_<uint32_t>(ADE_BVAHR);
+      this->ade_read_<uint32_t>(ADE_CVAHR);
+      this->ade_read_<uint32_t>(ADE_AFWATTHR);
+      this->ade_read_<uint32_t>(ADE_BFWATTHR);
+      this->ade_read_<uint32_t>(ADE_CFWATTHR);
+      this->ade_read_<uint32_t>(ADE_AFVARHR);
+      this->ade_read_<uint32_t>(ADE_BFVARHR);
+      this->ade_read_<uint32_t>(ADE_CFVARHR);
+      
+      // Enable the CF1, CF2, CF3 frequency convertor outputs
+      this->ade_write_<uint16_t>(ADE_CFMODE, 0x08A0); /* To double check */
+      
+      // Setup done
       this->is_setup_ = true;
     });
   }
