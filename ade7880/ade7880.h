61997a28fac874881f04788a8b120b4fc6ddd22a
Michaël Piron <4469877+michaelpiron@users.noreply.github.com>
2021-06-12 15:49:29 +0200

    Update ade7880.h

--- ade7880/ade7880.h
+++ ade7880/ade7880.h
@@ -4,6 +4,56 @@
 #include "esphome/components/i2c/i2c.h"
 #include "esphome/components/sensor/sensor.h"
 
+/** 
+ * ADE Defines
+ * =========== *
+ */
+#include "ade7880_registers.h"
+
+/** GPIO Defines --> TO CHECK!!! */ 
+#define ADE_PM0_GPIO        25
+#define ADE_PM1_GPIO        26
+#define ADE_RESET_GPIO      27
+#define ADE_GPIO_PIN_SEL    ((1ULL<<ADE_PM0_GPIO) | (1ULL<<ADE_PM1_GPIO) | (1ULL<<ADE_RESET_GPIO))
+
+/** I²C Defines --> TO CHECK!!! */
+#define SLAVE_7B_ADDRESS    0b0111000 /**< ADE7880 7-bit address, as described on the datasheet */
+#define I2C_PORT            I2C_NUM_0
+#define I2C_SDA_PIN         GPIO_NUM_21
+#define I2C_SCL_PIN         GPIO_NUM_22
+#define WRITE_BIT           I2C_MASTER_WRITE /*!< I2C master write */
+#define READ_BIT            I2C_MASTER_READ  /*!< I2C master read */
+#define ACK_CHECK_EN        0x1     /*!< I2C master will check ack from slave*/
+#define ACK_CHECK_DIS       0x0     /*!< I2C master will not check ack from slave */
+#define ACK_VAL             0x0         /*!< I2C ack value */
+#define NACK_VAL            0x1         /*!< I2C nack value */
+#define DEBUG               0
+
+/** PGA Gains, must be 0, 1, 2, 4, 8 or 16 */ 
+#define ADE_PGA_IGAIN       0   /**< PGA1 */
+#define ADE_PGA_NGAIN       0   /**< PGA2 */
+#define ADE_PGA_VGAIN       0   /**< PGA3 */
+#define ADE_USE_ROGOWSKI    0
+#define ADE_USE_60HZ        0   /** 50Hz in Europe, therefore 0 */
+
+/** Configuration register values --> TO CHECK!!! */
+#define ADE_PMAX_VAL        0x19CE5DE /**< From datasheet: instataneous power when inputs are at full scale */
+#define ADE_FS_VAL          1024000 /** Frequency at which the energy is accumulated */
+#define ADE_WTHR_VAL        0x03 /**< Default value. For this one and the following, see app note AN-1171, page 5 (Rev. A). */
+#define ADE_VARTHR_VAL      0x03 /**< Default value */
+#define ADE_VATHR_VAL       0x03 /**< Default value */    
+#define ADE_VLEVEL_VAL      0x38000 /**< Default value */
+#define ADE_VNOM_VAL        0x23C354  /** From eq. 42, VNOM = V/Vfs * 3766572 = 220*sqrt(2)/500 * 3766572 */
+#define ADE_CFXDEN_VAL      0x0DB3 /**< Based on AN-1171. Calibrate later. */
+
+/** ADE Readings --> TO CHECK!!!  */
+#define ADE_FULLSCALE_REG   5326737 /**< Max numeric value (positive/negative) corresponding to a max voltage at input */
+#define ADE_FULLSCALE_VAL   0.5f /**< Max voltage at the ADC input */
+#define ADE_VOLTAGE_ATT     (1.0f / 1001.0f) /**< Voltage attenuation */
+#define ADE_CURRENT_FULL    49.4975f /**< Peak full scale current. Irms = 35, Ip = 35 * sqrt(2) */
+#define ADE_PMAX            27059678 /**< Instantaneous power when inputs at full scale and in phase */
+
+
 namespace esphome {
 namespace ade7880 {
 
